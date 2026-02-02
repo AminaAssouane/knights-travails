35905bc80cabe6199cb54ca0c848780f58f7774d
Amina <minassouane@gmail.com>
2026-02-02 02:58:47 +0100

    Add BFS loop, working with a queue

--- knights-travails.js
+++ knights-travails.js
@@ -22,6 +22,27 @@ function neighbors(position) {
   return okMoves.map((move) => [position[0] + move[0], position[1] + move[1]]);
 }
 
-function move(position) {}
+function knightMoves(start, end) {
+  let queue = [[start, null]]; // Initializing the queue with the start square and putting null as its parent
+  let visited = [start]; // Marking it as visited
+  let found = false;
 
-function knightMoves(start, end) {}
+  while (queue.length > 0 && !found) {
+    // While our queue is not empty and we haven't found the square we want
+    let curr = queue.shift();
+    if (curr[0] === end)
+      found = true; // If it's the target, we stop
+    else if (!visited.includes(curr[0])) {
+      // We always check if the square hasn't been visited before we generate his neighbors
+      visited.push(curr[0]); // We mark it as visited
+      let neighbors = neighbors(curr[0]); // We generate his neighbors
+      for (let i = 0; i < neighbors.length(); i++) {
+        // We enqueue the non visited neighbors
+        if (!visited.includes(neighbors[i])) {
+          queue.push([neighbors[i], curr]);
+          visited.push(neighbors[i]);
+        }
+      }
+    }
+  }
+}
